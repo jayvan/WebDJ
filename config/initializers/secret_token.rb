3776ed292e543f2dd64efa9b28b0af98e12bb2c9
Justin Vanderheide <justin@justinvanderheide.com>
2012-11-02 09:51:03 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebDJ::Application.config.secret_token = 'c185844fc3ffa716bf23287a79fd51d9480cad9f808c0365179f756183b18f67a628d0e15a940195bedb4503fc5fd69f1de8486354bfb61f01ce1a8be6292542'
