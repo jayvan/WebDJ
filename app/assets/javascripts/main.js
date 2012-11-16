45f9a45bc253b6eeb0e3a5ea01fb12be5765d54a
Justin Vanderheide <justin@justinvanderheide.com>
2012-11-16 13:27:43 -0500

    Form for adding songs to the queue

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,8 +3,7 @@ require([
 ], function(
   Room
 ){
-  var roomId = window.location.pathname.split("/").slice(-1)[0];
+  var roomId = window.location.pathname.split("/")[2];
   var room = new Room(roomId);
   ko.applyBindings(room);
 });
-
--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -26,24 +26,45 @@ define([
         }
       }
     });
+    self.baseURL = "/rooms/" + self.id + "/";
     self.lastUpdate = 0;
+    self.fetchTimeout = undefined;
     self.fetchData();
   };
 
   Room.prototype.fetchData = function() {
     var self = this;
-    $.getJSON("/rooms/" + self.id + "/queue.json?last_update=" + self.lastUpdate, function(data) {
+    $.getJSON(self.baseURL + "queue.json?last_update=" + self.lastUpdate, function(data) {
       data.forEach (function(song) {
-        if (song.play_at >= Utils.time() - song.duration) {
+        if (song.play_at + song.duration > Utils.time()) {
           self.songs.push(ko.observable(new Song(song.provider, song.identifier, song.play_at)));
         }
       });
 
-      window.setTimeout(function() {
+      clearTimeout(self.fetchTimeout);
+      self.fetchTimeout = window.setTimeout(function() {
         self.fetchData();
       }, FETCH_INTERVAL);
     });
     self.lastUpdate = Utils.time();
+  };
+
+  Room.prototype.enqueueSong = function(form) {
+    var self = this;
+    var $form = $(form);
+    var $submitButton = $form.find('[type="submit"]');
+
+    $submitButton.attr('disabled', true);
+    $submitButton.val("Adding...");
+
+    $.post(this.baseURL + "enqueue", $form.serialize(),
+      function(data) {
+        console.log(data);
+        $submitButton.attr('disabled', false);
+        $submitButton.val("Add Song");
+        $form[0].reset();
+        self.fetchData();
+      });
   };
 
   return Room;
