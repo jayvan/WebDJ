01e04094272711a009bab898ebe92d7f6893033b
Justin Vanderheide <justin@justinvanderheide.com>
2012-11-19 23:07:34 -0500

    include user in redis uri

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 uri = URI.parse(ENV["REDIS_URL"] || "localhost")
-$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :user => uri.user)
