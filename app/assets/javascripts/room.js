c7fa9d61cdf8ece76e6ec1306615488b6c997f70
Justin Vanderheide <justin@justinvanderheide.com>
2012-11-23 17:03:44 -0500

    Prevent adding multiple copies of the same song

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -54,7 +54,7 @@ define([
           data.forEach (function(song) {
             if (song.playAt + song.duration > Utils.time()) {
               var newSong = new Song(song);
-              self.songs.push(ko.observable(newSong));
+              self.pushSongToQueue(newSong);
               newSong.load();
             }
           });
@@ -71,6 +71,17 @@ define([
       self.lastUpdate = Utils.time();
     };
   })();
+
+  // Makes sure that the song is unique and adds it to the queue
+  Room.prototype.pushSongToQueue = function(song) {
+    for (var i = 0; i < this.songs().length; i++) {
+      if (song.startAt === this.songs()[i]().startAt) {
+        return;
+      }
+    }
+
+    this.songs.push(ko.observable(song));
+  };
 
   Room.prototype.enqueueSong = function(form) {
     var self = this;
