9ae3da06c55fffc52cff4240b97d7f3e0fd77a27
Justin Vanderheide <justin@justinvanderheide.com>
2012-12-07 19:26:58 -0500

    Link logo to home page. Add randomized names for active users. Unix line endings in the css files

---
+++ app/helpers/room_helper.rb
@@ -0,0 +1,19 @@
+module RoomHelper
+  SAYINGS = [
+    "dancing ninja",
+    "idol reject",
+    "tipsy pirate",
+    "raving samurai",
+    "romantic parisian",
+    "viking warrior",
+    "happy penguin",
+    "exotic dinosaur",
+    "party rocker",
+    "belieber"
+  ]
+
+  def user_saying
+    SAYINGS.sample
+  end
+
+end
