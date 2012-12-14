9004ba9c54d5fb734c5e806a4d0d05a5e865e9e8
Justin Vanderheide <justin@justinvanderheide.com>
2012-12-14 13:31:56 -0500

    Have a bit more left margin for large test

--- app/assets/javascripts/misc/reveal_header.js
+++ app/assets/javascripts/misc/reveal_header.js
@@ -1,7 +1,7 @@
 $(function() {
   var setHeaderWidth = function() {
     $('h1').each(function(i, box) {
-      var width = window.innerWidth < 960 ? window.innerWidth : window.innerWidth - 360,
+      var width = window.innerWidth < 960 ? window.innerWidth : window.innerWidth - 370,
           html = '<span style="white-space:nowrap">',
           line = $(box).wrapInner(html).children()[0],
           n = 120;
